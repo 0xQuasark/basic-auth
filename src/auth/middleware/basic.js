eebb6cd35a0ebf4a5d7e94b047132a16be9c83a3
Quasark <q@quasark.xyz>
2023-09-26 15:07:04 -0700

    tests included

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -10,7 +10,7 @@ async function basicAuth(req, res, next) {
     if (req.body.password) {req.body.password = await bcrypt.hash(req.body.password, 10);}
     next();
   } else if (req.path === '/signin') {
-    console.log('basic.js: signin', req.body)
+    // console.log('basic.js: signin', req.body)
     let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'am9objpmb28=']
     let encodedString = basicHeaderParts.pop();  // am9objpmb28=
     let decodedString = base64.decode(encodedString); // "username:password"
